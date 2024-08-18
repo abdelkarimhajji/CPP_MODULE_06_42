edb0ded202dd1ccc8da7f67d45bda852ad960535
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2024-08-18 07:42:33 +0100

    i almost finish exercice

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,7 +6,27 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 15:47:53 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/17 15:47:54 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/08/18 07:40:47 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Serializer.hpp"
+
+Serializer::Serializer()
+{
+    
+}
+Serializer::~Serializer()
+{
+    
+}
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* Serializer::deserialize(uintptr_t raw)
+{
+    return reinterpret_cast<Data*>(raw);
+}
--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -6,7 +6,29 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 15:47:55 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/17 15:47:56 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/08/18 07:17:25 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include <cstdint>
+#include <iostream>
+
+struct Data{
+    int id;
+};
+
+class Serializer
+{
+    private:
+        Serializer();
+        ~Serializer();
+        
+    public:
+        static uintptr_t serialize(Data* ptr);
+        static Data* deserialize(uintptr_t raw);
+};
+
+#endif
